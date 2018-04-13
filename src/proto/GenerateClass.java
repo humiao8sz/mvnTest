7bf895b32201163497e74749096f3369982c9a5b
humiao <humiao@bojoy-humiao.bojoy.net>
2018-04-13 15:10:29 +0800

    111

--- src/proto/GenerateClass.java
+++ src/proto/GenerateClass.java
@@ -3,18 +3,18 @@ package proto;
 import java.io.IOException;
 
 public class GenerateClass {
-    public static void main(String[] args) {
-    	//protoc --java_out=./java/ ./proto/helloworld.proto
-    	// -I=D:/workspace2/mvnTest/src
-    	// -I=D:/workspace2/mvnTest/src --java_out=D:/workspace2/mvnTest/src D:/workspace2/mvnTest/src/BbjxMsg.proto
-        String protoFile = "BbjxMsg.proto";//  D:\WorkSpace\TT\src\proto
-        String strCmd = "D:/lib/proto/protobuf-master/src/protoc.exe -I=D:/workspace2/mvnTest/src --java_out=D:/workspace2/mvnTest/src D:/workspace2/mvnTest/src/"+ protoFile;
-        
-       //String strCmd = "D:/lib/proto/protoc.exe D:/workspace2/mvnTest/src/proto/"+ protoFile;
-        try {
-            Runtime.getRuntime().exec(strCmd);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }  
-    }
+	public static void main(String[] args) {
+		// protoc --java_out=./java/ ./proto/helloworld.proto
+		// -I=D:/workspace2/mvnTest/src
+		// -I=D:/workspace2/mvnTest/src --java_out=D:/workspace2/mvnTest/src D:/workspace2/mvnTest/src/BbjxMsg.proto
+		// String protoFile = "BbjxMsg.proto";// D:\WorkSpace\TT\src\proto
+		// String strCmd = "D:/lib/proto/protoc.exe D:/workspace2/mvnTest/src/proto/"+ protoFile;
+		String strCmd =
+				"D:/lib/proto/protobuf-master/src/protoc.exe -I=D:/workspace2/mvnTest/src --java_out=D:/workspace2/mvnTest/src D:/workspace2/mvnTest/src/BbjxMsg.proto";
+		try{
+			Runtime.getRuntime().exec(strCmd);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
 }
