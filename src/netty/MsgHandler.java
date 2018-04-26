1a25502dd75694cb0a0dd0217cf86169733ba963
humiao <humiao@bojoy-humiao.bojoy.net>
2018-04-26 13:28:46 +0800

    1

--- src/netty/MsgHandler.java
+++ src/netty/MsgHandler.java
@@ -10,8 +10,8 @@ public class MsgHandler extends SimpleChannelInboundHandler<ByteBuf> {
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
-		ctx.write(null);
-		ctx.channel().write(null);
+		ctx.write(null);//写到ChannelPipeline中当前ChannelHandler的下一个ChannelHandler
+		ctx.channel().write(null);//写到ChannelPipeline中最后一个ChannelHandler然后数据从尾部开始向头部方向流动会经过所有的ChannelHandler
 		msg.writeInt(1);
 		ctx.writeAndFlush(new Object()).addListener(new ChannelFutureListener() {
 			@Override
