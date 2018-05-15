cc5f8a57f0662d68d276101a68b04797b5fd38e1
humiao <humiao@bojoy-humiao.bojoy.net>
2018-05-15 17:05:08 +0800

    1

---
+++ src/webmagic/MyFilePipeline.java
@@ -0,0 +1,48 @@
+package webmagic;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import us.codecraft.webmagic.ResultItems;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.pipeline.FilePipeline;
+
+public class MyFilePipeline extends FilePipeline {
+	private Logger logger = LoggerFactory.getLogger(getClass());
+    public MyFilePipeline(String path) {
+        super(path);
+    }
+    
+    @Override
+    public void process(ResultItems resultItems, Task task) {
+        String path = this.path + PATH_SEPERATOR + task.getUUID() + PATH_SEPERATOR;
+        try {
+            PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(getFile(path + DigestUtils.md5Hex(resultItems.getRequest().getUrl()) + ".html")),"UTF-8"));
+            printWriter.format("UTF-8");
+            printWriter.println("url:\t" + resultItems.getRequest().getUrl());
+            for (Map.Entry<String, Object> entry : resultItems.getAll().entrySet()) {
+                if (entry.getValue() instanceof Iterable) {
+                    Iterable value = (Iterable) entry.getValue();
+                    printWriter.print(entry.getKey() + ":\t");
+                    for (Object o : value) {
+                        printWriter.print(o+"\t");
+                    }
+                    printWriter.println();
+                } else {
+                    printWriter.println(entry.getKey() + ":\t" + entry.getValue());
+                }
+            }
+            printWriter.close();
+        } catch (IOException e) {
+            logger.warn("write file error", e);
+        }
+    }
+
+}
--- src/webmagic/ZhihuPageProcessor.java
+++ src/webmagic/ZhihuPageProcessor.java
@@ -1,9 +1,14 @@
 package webmagic;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.alibaba.fastjson.JSONObject;
+
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
-import us.codecraft.webmagic.pipeline.ConsolePipeline;
+import us.codecraft.webmagic.downloader.selenium.SeleniumDownloader;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 /**
@@ -13,23 +18,32 @@ import us.codecraft.webmagic.processor.PageProcessor;
 public class ZhihuPageProcessor implements PageProcessor {
 
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
-
+    private static int i = 0;
     @Override
     public void process(Page page) {
-        page.addTargetRequests(page.getHtml().links().regex("https://www\\.zhihu\\.com/question/\\d+/answer/\\d+.*").all());
+//      page.addTargetRequests(page.getHtml().links().regex("https://www\\.zhihu\\.com/question/\\d+.*").all());
 /*      page.putField("title", page.getHtml().xpath("//h2[@class='zm-item-title']/a/text()").toString());
         page.putField("question", page.getHtml().xpath("//div[@id='zh-question-detail']//tidyText()").toString());
         page.putField("answer", page.getHtml().xpath("//div[@id='zh-question-answer-wrap']//div[@class='zm-editable-content']/tidyText()").toString());
-*/        
-        page.putField("title", page.getHtml().xpath("//a[@href]/text()").toString());
-//        page.putField("question", page.getHtml().xpath("//textarea[1]").toString());
-//        page.putField("answer", page.getHtml().xpath("//body//a[1]").toString());
-        
-        if (page.getResultItems().get("title")==null){
-            //skip this page
-            page.setSkip(true);
-        }
-    }
+*/     
+       String title = "";
+       List<String> list = new ArrayList<>();
+       for(String data : page.getHtml().xpath("//div[@class='ContentItem AnswerItem']/@data-zop").all()){
+    	   JSONObject da = JSONObject.parseObject(data);    	 
+    	   list.add((String)da.get("authorName"));
+    	   title = (String)da.get("title");
+       }
+       page.putField("title", title);
+       page.putField("autor", list);
+//     page.putField("answer", page.getHtml().xpath("//body//a[1]").toString());
+       
+       List<String> urllist = new ArrayList<>();
+       for(String data : page.getHtml().xpath("//div[@class='SimilarQuestions-item']//meta[@itemprop='url']/@content").all()){
+    	   urllist.add(data);
+       }
+   	   page.addTargetRequests(urllist);
+           
+	}
 
     @Override
     public Site getSite() {
@@ -37,7 +51,10 @@ public class ZhihuPageProcessor implements PageProcessor {
     }
 
     public static void main(String[] args) {
-        Spider.create(new ZhihuPageProcessor()).addUrl("https://www.zhihu.com/explore").addPipeline(new ConsolePipeline()).run();
+        System.setProperty("selenuim_config", "D:\\workspace2\\mvnTest\\config.ini");
+        Spider.create(new ZhihuPageProcessor()).addUrl("https://www.zhihu.com/question/38515926").addPipeline(new MyFilePipeline("D:\\workspace2\\mvnTest"))
+        .downloader(new MySeleniumDownloader("D:\\workspace2\\mvnTest\\chromedriver.exe"))
+        .run();
     }
 }
 
